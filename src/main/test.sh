b9f3c1d6d07b551c3d47459e734f5de16782b9b8
endfighting <hggend@gmail.com>
2017-08-06 05:08:27 +0000

    for test

--- src/main/test.sh
+++ src/main/test.sh
@@ -1,14 +1,5 @@
-ipcrm -S 7803
-ipcrm -S 7801
-ipcrm -S 7802
-ipcrm -S 15606
-ipcrm -S 15602
-ipcrm -S 15604
-ipcrm -S 23409
-ipcrm -S 23403
-ipcrm -S 23406
+ipcrm -S 7800
+ipcrm -S 15600
+ipcrm -S 23400
 
-ipcrm -Q 7801
-ipcrm -Q 7802
-ipcrm -Q 7803
-
+ipcrm -Q 7800
